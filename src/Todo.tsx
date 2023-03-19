4ca7045a5596b5dba417016df0f7dae026e8e18f
Victor O Santos <victor.engsantos@gmail.com>
2023-03-19 17:16:09 -0300

    RT-2 - Delete task button

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -71,6 +71,13 @@ export default function Todo({ type }: Props) {
     setTodoItemsOnLocalStorage(updatedTodoItems);
   }
 
+  function deleteTask(taskId: string) {
+    const updatedTodoItems = todoItems.filter(item => item.key !== taskId);
+
+    setTodoItems(updatedTodoItems);
+    setTodoItemsOnLocalStorage(updatedTodoItems);
+  }
+
   window.addEventListener('load', function () {
     initializeTodo();
   })
@@ -80,10 +87,16 @@ export default function Todo({ type }: Props) {
       <ul className='todo-list'>
         {todoItems.map((item, index) => {
           return (
-            <li key={item.key} onClick={() => toggleCheckbox(item.key)}>
-              <input className='checkbox' type='checkbox' checked={item.checked} readOnly/>
-              <span className={item.checked ? 'text-muted' : ''}>{item.activity}</span>
-              <hr />
+            <li className="todo-list-item" key={item.key}>
+              <span className="todo-list-item-clickable" onClick={() => toggleCheckbox(item.key)}>
+                <input className='checkbox' type='checkbox' checked={item.checked} readOnly/>
+                <span className={'todo-list-item-text ' + (item.checked ? 'text-muted' : '')}>{item.activity}</span>
+              </span>
+              <div className="todo-list-item-delete" onClick={() => deleteTask(item.key)}>
+                <svg fill='currentColor' width='20' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                  <path d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"/>
+                </svg>
+              </div>
             </li>
           )
         })}
